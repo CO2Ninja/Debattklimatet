4f2235e8cb4ceb2b223a340f6a9d96c32ef0fdfc
Daniel Karlsson <danieka@kth.se>
2014-04-21 11:22:05 +0200

    More bugfixes

--- django_backend/debattklimatet/models.py
+++ django_backend/debattklimatet/models.py
@@ -27,6 +27,35 @@ from django.db import models
     #Truncated            bool        `json:"truncated"`
     #User                 User        `json:"user"`
 #}
+
+class User(models.Model):
+    Id                  = models.BigIntegerField(primary_key = True)
+    Name                = models.CharField(max_length = 200)
+    ScreenName          = models.CharField(max_length = 200)
+    ProfileImageUrl     = models.CharField(max_length = 200)
+    
+class HashTag(models.Model):
+    tag                 = models.CharField(max_length = 100, primary_key = True)
+
+
+#Media []struct {
+        #Id              int64
+        #Id_str          Char
+        #Media_url       Char
+        #Media_url_https Char
+        #Url             Char
+        #Display_url     Char
+        #Expanded_url    Char
+        #Sizes           MediaSizes
+        #Type            Char
+        #Indices         []int
+    #}
+class Media(models.Model):
+    Id                  = models.BigIntegerField(primary_key = True)
+    Media_url           = models.CharField(max_length = 200)
+    Media_url_https     = models.CharField(max_length = 200)
+    Url                 = models.CharField(max_length = 200)
+
 class Tweet(models.Model):
     CreatedAt           = models.DateTimeField(db_index = True)
     FavoriteCount       = models.IntegerField()
@@ -39,7 +68,6 @@ class Tweet(models.Model):
     #InReplyToUserID     = models.BigInteger(Field)
     RetweetCount        = models.IntegerField()
     Retweeted           = models.BooleanField()
-    RetweetedStatus     = models.ForeignKeyField(Tweet)
     Source              = models.CharField(max_length = 200)
     Text                = models.CharField(max_length = 150)
     User                = models.ForeignKey(User)
@@ -87,32 +115,6 @@ class Tweet(models.Model):
     #UtcOffset                      int    `json:"utc_offset"`
     #Verified                       bool   `json:"verified"`
 #}
-class User(models.Model):
-    Id                  = models.BigIntegerField(primary_key = True)
-    Name                = models.CharField(max_length = 200)
-    ScreenName          = models.CharField(max_length = 200)
-    ProfileImageUrl     = models.CharField(max_length = 200)
-    
-class HashTag(models.Model):
-    tag                 = models.CharField(max_length = 100, primary_key = True)
 
-
-#Media []struct {
-        #Id              int64
-        #Id_str          Char
-        #Media_url       Char
-        #Media_url_https Char
-        #Url             Char
-        #Display_url     Char
-        #Expanded_url    Char
-        #Sizes           MediaSizes
-        #Type            Char
-        #Indices         []int
-    #}
-class Media(models.Model):
-    Id                  = models.BigIntegerField(primary_key = True)
-    Media_url           = models.CharField(max_length = 200)
-    Media_url_https     = models.CharField(max_length = 200)
-    Url                 = models.CharField(max_length = 200)
     
     
